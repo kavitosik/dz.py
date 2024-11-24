36f5283b1881c8259138c3b691dfc3d157f12a63
Kirill <km220410@gmail.com>
2024-11-24 09:20:45 +0000

    задание 3 сделал

--- dz9/task3.py
+++ dz9/task3.py
@@ -4,11 +4,10 @@ for i in range(100, 9999):
     i = str(i)
     x = len(i)
     if x == 3:
-        if i[0] != i[1] != i[2]:
+        if i[0] != i[1] != i[2] and i[0] != i[2]:
             total += 1
-            print(i)
     elif x == 4:
-        if i[0] != i[1] != i[2] != i[3]:
+        if i[0] != i[1] != i[2] != i[3] and i[0] != i[2] and i[0] != i[3] and i[1] != i[3]:
             total += 1
-            print(i)
+
 print(total)
