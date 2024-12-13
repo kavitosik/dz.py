885d3e69976a5b9197c5d2f4c71981e57233d65a
bobrdobrz <spulkagulka221@gmail.com>
2024-12-13 20:24:11 +0300

    задание 10 выполнил

---
+++ dz_na_yroke/task10.py
@@ -0,0 +1,4 @@
+num = input("Введите любое четырёхзначное число: ")
+sum = int(num[0]) + int(num[1]) + int(num[2]) + int(num[3])
+mult = int(num[0]) * int(num[1]) * int(num[2]) * int(num[3])
+print(f"Сумма цифр: {sum}\nПроизведение цифр: {mult}")
