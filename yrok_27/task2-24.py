c6d50627f4359854b174ff08b77b83536c849716
bobrdobrz <spulkagulka221@gmail.com>
2024-12-27 21:18:30 +0300

    задание 2-24 выполнил

---
+++ yrok_27/task2-24.py
@@ -0,0 +1,6 @@
+num_end = 237
+num_progress1 = num_end * 10
+num_progress2 = str(num_progress1)[0]
+num_progress3 = num_progress1 + int(num_progress2)
+num_otvet = str(num_progress3)[1:]
+print(num_otvet)
